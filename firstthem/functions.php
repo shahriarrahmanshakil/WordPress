53491093521f41bab695bf1dfb6374ce09ab629e
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-06-03 12:21:06 +0600

    functions.php added

--- firstthem/functions.php
+++ firstthem/functions.php
@@ -10,6 +10,17 @@
     //theme css and jquery file calling 
     function css_js_file_calling(){       
         wp_enqueue_style('shakil-style', get_stylesheet_uri());
+        wp_register_style('bootstrap',get_template_directory_uri().'/css/bootstrap.css', array(), '5.3.3', 'all');
+        wp_register_style('custom',get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all');
+        wp_enqueue_style('bootstrap');
+        wp_enqueue_style('custom');
+        
+        // jquery 
+        wp_enqueue_script('jquery');
+        wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '5.3.3', 'true');
+        wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', array(), '5.3.3', 'true');
+        
+
 
     }
     add_action('wp_enqueue_scripts','css_js_file_calling');
